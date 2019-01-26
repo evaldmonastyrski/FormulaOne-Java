df2e9f9ec584b58b7a771207d7d5c144c74e6e73
Evald Monastyrski <evaldmon@gmail.com>
2019-01-26 12:19:15 +0100

    evaldmon: Add Logo
    Support both Windows and Mac OS X

---
+++ simulator-application/src/main/java/gui/GuiMainUtil.java
@@ -0,0 +1,23 @@
+package gui;
+
+import org.jetbrains.annotations.NotNull;
+
+enum GuiMainUtil {
+    ;
+
+    static boolean isApple()
+    {
+        return getOsName().startsWith("Mac OS X");
+    }
+
+    static boolean isWindows()
+    {
+        return getOsName().startsWith("Windows");
+    }
+
+    @NotNull
+    private static String getOsName()
+    {
+        return System.getProperty("os.name");
+    }
+}
