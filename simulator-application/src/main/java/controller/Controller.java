44b0e3543f2ec248788b6830daa40924f1d1dd7b
Evald Monastyrski <evaldmon@gmail.com>
2019-02-02 19:33:42 +0100

    evaldmon: Store base components in Controller

--- simulator-application/src/main/java/controller/Controller.java
+++ simulator-application/src/main/java/controller/Controller.java
@@ -1,9 +1,13 @@
 package controller;
 
+import controller.deserializer.BaseDriver;
+import controller.deserializer.BaseTeam;
 import controller.deserializer.Deserializer;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 class Controller {
 
@@ -12,9 +16,14 @@ class Controller {
     @NotNull private final GuiController guiController;
     @NotNull private final Deserializer deserializer;
 
+    @NotNull private final List<BaseDriver> baseDrivers;
+    @NotNull private final List<BaseTeam> baseTeams;
+
     Controller() {
         guiController = new GuiController(this);
         deserializer = new Deserializer();
+        baseDrivers = deserializer.getDrivers();
+        baseTeams = deserializer.getTeams();
         initialize();
     }
 
