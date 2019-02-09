1f5734147d9d111ab370be727de0bbe8d1ec83f6
Evald Monastyrski <evaldmon@gmail.com>
2019-02-09 23:18:46 +0100

    evaldmon: Add ComboBox management

--- simulator-application/src/main/java/gui/GuiConstants.java
+++ simulator-application/src/main/java/gui/GuiConstants.java
@@ -6,4 +6,6 @@ public enum GuiConstants {
     public static final int NUMBER_OF_DRIVERS = 20;
     public static final int NUMBER_OF_TEAMS = 10;
     public static final int NUMBER_OF_ENGINES = 4;
+    public static final int QUALIFICATION_AWARDED_PLACES = 8;
+    public static final int RACE_AWARDED_PLACES = 12;
 }
--- simulator-application/src/main/java/gui/setuppanel/SetupComboBoxManager.java
+++ simulator-application/src/main/java/gui/setuppanel/SetupComboBoxManager.java
@@ -3,6 +3,7 @@ package gui.setuppanel;
 import gui.GuiConstants;
 import gui.SetupPanel;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.JComboBox;
 import java.awt.GridBagConstraints;
@@ -10,10 +11,17 @@ import java.awt.Insets;
 import java.util.ArrayList;
 import java.util.List;
 
+import static gui.setuppanel.ComboBoxManagerUtil.newNumberSelected;
+import static gui.setuppanel.ComboBoxManagerUtil.numberReplaced;
+import static gui.setuppanel.ComboBoxManagerUtil.numberRemoved;
+
+@SuppressWarnings({"NullPointerException", "All"})
 public class SetupComboBoxManager {
 
     @NotNull private final List<JComboBox<Integer>> driverQualificationPositions = new ArrayList<>();
     @NotNull private final List<JComboBox<Integer>> driverRacePositions = new ArrayList<>();
+    @NotNull private final Integer @Nullable [] qualificationCache = new Integer[GuiConstants.NUMBER_OF_DRIVERS];
+    @NotNull private final Integer @Nullable [] raceCache = new Integer[GuiConstants.NUMBER_OF_DRIVERS];
 
     @NotNull private final SetupPanel setupPanel;
     @NotNull private final GridBagConstraints constraints;
@@ -23,31 +31,53 @@ public class SetupComboBoxManager {
         this.constraints = constraints;
 
         for (int i = 0; i < GuiConstants.NUMBER_OF_DRIVERS; i++) {
-            driverQualificationPositions.add(new JComboBox<>());
-            driverRacePositions.add(new JComboBox<>());
+            driverQualificationPositions.add(new JComboBox<>(new SortedComboBoxModel()));
+            driverRacePositions.add(new JComboBox<>(new SortedComboBoxModel()));
         }
     }
 
     public void init() {
-        initializeComboBoxes(constraints, driverQualificationPositions, 1);
-        initializeComboBoxes(constraints, driverRacePositions, 2);
+        initializeComboBoxes(constraints, driverQualificationPositions, CompetitionType.QUALIFICATION, 1);
+        initializeComboBoxes(constraints, driverRacePositions,CompetitionType.RACE, 2);
     }
 
     private void initializeComboBoxes(@NotNull GridBagConstraints constraints,
                                       List<JComboBox<Integer>> driverPositions,
+                                      CompetitionType type,
                                       int columnNo) {
         int rowNo = 0;
         for (JComboBox<Integer> driverPosition : driverPositions) {
+            final int cacheIndex = rowNo;
             constraints.fill = GridBagConstraints.CENTER;
             constraints.gridx = columnNo;
             constraints.gridy = rowNo;
             constraints.insets = new Insets(1, 0, 1, 0);
             driverPosition.setMaximumRowCount(GuiConstants.NUMBER_OF_DRIVERS);
-            for (int j = 1; j < GuiConstants.NUMBER_OF_DRIVERS + 1; j++) {
+            driverPosition.addItem(null);
+            int awardedPlaces = type == CompetitionType.QUALIFICATION
+                    ? GuiConstants.QUALIFICATION_AWARDED_PLACES
+                    : GuiConstants.RACE_AWARDED_PLACES;
+            for (Integer j = 1; j <= awardedPlaces; j++) {
                 driverPosition.addItem(j);
             }
+            driverPosition.addActionListener(e ->
+                    updateComboBox(cacheIndex, driverPosition, type));
             setupPanel.add(driverPosition, constraints);
             rowNo++;
+        }
+    }
+
+    private void updateComboBox(int cacheIndex, @NotNull JComboBox<Integer> receivedCB, CompetitionType type) {
+        @Nullable Integer position = (Integer) receivedCB.getSelectedItem();
+
+        if (type == CompetitionType.QUALIFICATION) {
+            newNumberSelected(cacheIndex, receivedCB, position, qualificationCache, driverQualificationPositions);
+            numberReplaced(cacheIndex, receivedCB, position, qualificationCache, driverQualificationPositions);
+            numberRemoved(cacheIndex, receivedCB, position, qualificationCache, driverQualificationPositions);
+        } else {
+            newNumberSelected(cacheIndex, receivedCB, position, raceCache, driverRacePositions);
+            numberReplaced(cacheIndex, receivedCB, position, raceCache, driverRacePositions);
+            numberRemoved(cacheIndex, receivedCB, position, raceCache, driverRacePositions);
         }
     }
 }
